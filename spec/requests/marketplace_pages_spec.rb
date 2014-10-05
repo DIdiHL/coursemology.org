38430fabfe18de366e0023973b254bac2470f09f
Han Lin <hanlin.dev@gmail.com>
2014-10-05 20:51:55 +0800

    Remove marketplace page tests.

--- spec/requests/marketplace_pages_spec.rb
+++ spec/requests/marketplace_pages_spec.rb
@@ -1,19 +1,5 @@
 require "spec_helper"
 
 describe "MarketplacePages" do
-  subject { page }
-  let(:admin) { FactoryGirl.create(:admin) }
-  let(:course) { FactoryGirl.create(:course) }
-
-  before do
-    sign_in admin
-    visit marketplace_index_path
-  end
-
-  it { should have_link("site-logo", href:marketplace_index_path) }
-
-  describe "Marketplace Index" do
-    it { should have_field("search_keywords") }
-  end
-
+  #TODO implement tests
 end
